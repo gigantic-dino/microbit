565ced2176a9a0491c3e851d1b90e60bbd8cb06f
gigantic-dino <76637371+gigantic-dino@users.noreply.github.com>
2020-12-28 13:21:41 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,12 +3,12 @@ bluetooth.onBluetoothConnected(function () {
     bluetooth.uartWriteLine("Microbit connected")
     connected = 1
     while (connected == 1) {
-        rec_data = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash))
-        serial.writeString(rec_data)
-        serial.writeLine("")
-        serial.writeValue("status", connected)
+        rec_data = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+        // serial.writeString(rec_data)
+        // serial.writeLine("")
+        // serial.writeValue("status", connected)
         send_data = "Received ###" + rec_data + "###"
-        bluetooth.uartWriteString(send_data)
+        bluetooth.uartWriteLine(send_data)
         basic.pause(100)
     }
 })
@@ -16,15 +16,23 @@ bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.Sad)
     connected = 0
 })
+input.onButtonPressed(Button.A, function () {
+    if (connected != 0) {
+        bluetooth.uartWriteLine("Button A pressed\n")
+    }
+})
+/**
+ * serial.redirectToUSB()
+ */
 let send_data = ""
 let rec_data = ""
 let connected = 0
-bluetooth.startAccelerometerService()
-bluetooth.startButtonService()
-bluetooth.startIOPinService()
-bluetooth.startLEDService()
-bluetooth.startTemperatureService()
-bluetooth.startMagnetometerService()
+// bluetooth.startAccelerometerService()
+// bluetooth.startButtonService()
+// bluetooth.startIOPinService()
+// bluetooth.startLEDService()
+// bluetooth.startTemperatureService()
+// bluetooth.startMagnetometerService()
 bluetooth.startUartService()
 basic.showIcon(IconNames.Square)
 basic.pause(200)
@@ -39,4 +47,3 @@ basic.showLeds(`
     `)
 basic.pause(200)
 basic.clearScreen()
-serial.redirectToUSB()
